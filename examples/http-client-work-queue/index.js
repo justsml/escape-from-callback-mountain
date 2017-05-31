79b646694a89abaaffda81a63676ba9cd1956108
Daniel Levy <justsml@users.noreply.github.com>
2017-05-30 21:17:32 -0600

    Adding initial example folder content
    See wiki examples section for more info

---
+++ examples/http-client-work-queue/index.js
@@ -0,0 +1,9 @@
+const Promise           = require('bluebird')
+const {hashStringAsync} = Promise.promisifyAll(require('./lib/crypto'))
+const {logEventAsync}   = Promise.promisifyAll(require('./lib/log'))
+const {openAsync}       = Promise.promisifyAll(require('./lib/db'))
+
+
+const _dbHandle = openAsync()
+
+
