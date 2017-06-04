b9b5eb23798eec47c84c2eec177213b2ee18e39f
Dan Levy <dan@danlevy.net>
2017-06-04 16:00:09 -0600

    re-org folders, adding src dir for code

--- src/lib/log.test.js
+++ src/lib/log.test.js
@@ -1,10 +1,19 @@
 const test = require('ava')
 const {logEvent, logEventAsync} = require('./log')
 
-test('log.logEventAsync', t => {
+test('logEventAsync', t => {
   t.plan(2)
   return logEventAsync({event: 'login', username: 'eve'})
   .tap(result => {
+    t.truthy(result.success)
+    t.pass()
+  })
+})
+
+test('logEvent', t => {
+  t.plan(3)
+  return logEvent({event: 'login', username: 'eve'}, (err, result) => {
+    t.ifError(err)
     t.truthy(result.success)
     t.pass()
   })
