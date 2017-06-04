98eca917d08b4f134b1686e833a577a945db253f
Daniel Levy <justsml@users.noreply.github.com>
2017-06-03 18:41:45 -0600

    Error factory pattern for typed error support (#13)
    * Error factory pattern for typed error support
    
    * fixes #7: adds typed error handlers

--- examples/typed-errors/auth.js
+++ examples/typed-errors/auth.js
@@ -5,13 +5,19 @@ const Promise           = require('bluebird')
 const {hashStringAsync} = Promise.promisifyAll(require('./lib/crypto'))
 const {logEventAsync}   = Promise.promisifyAll(require('./lib/log'))
 const {openAsync}       = Promise.promisifyAll(require('./lib/db'))
+const {TimeoutError,
+  ValidationError,
+  NotFoundError }       = require('./errors')
 
-var _dbPromise = openAsync() // FYI: Promises include memoization (caching) built into same API
+const _openHandle = openAsync(); // FYI: Promises include memoization (caching) built into same API
 
-module.exports = {auth}
+module.exports = {auth};
 
 /* auth is our main function */
-function auth({username, password}) {
+function auth({username, password,
+    _onTimeoutError = errorHandler,
+    _onNotFoundError = errorHandler,
+    _onValidationError = errorHandler}) {
   return Promise.resolve({username, password})
     .then(authValidated)
     .tap(logEventAsync({event: 'login', username}))
@@ -21,17 +27,20 @@ function auth({username, password}) {
         .then(users.findOneAsync)
         .then(userFound)
     })
+    .catch(TimeoutError,    _onTimeoutError)
+    .catch(NotFoundError,   _onNotFoundError)
+    .catch(ValidationError, _onValidationError)
     .catch(errorHandler)
 }
 
 function authValidated({username, password}) {
-  if (!username || username.length < 4) { return Promise.reject(new Error('Invalid username. Required, 4 char minimum.')) }
-  if (!password || password.length < 4) { return Promise.reject(new Error('Invalid password. Required, 4 char minimum.')) }
+  if (!username || username.length < 4) { return Promise.reject(new ValidationError('Invalid username. Required, 4 char minimum.')) }
+  if (!password || password.length < 4) { return Promise.reject(new ValidationError('Invalid password. Required, 4 char minimum.')) }
   return {username, password}
 }
 
 function userFound(results) {
-  return results ? results : Promise.reject(new Error('No users matched. Login failed'))
+  return results ? results : Promise.reject(new NotFoundError('No users matched. Login failed'))
 }
 
 function hashedPasswordUserQuery({username, password}) {
@@ -42,7 +51,7 @@ function hashedPasswordUserQuery({username, password}) {
 }
 
 function usersModel() {
-  return _dbPromise
+  return _openHandle
     .then(({models}) => models.users)
 }
 
