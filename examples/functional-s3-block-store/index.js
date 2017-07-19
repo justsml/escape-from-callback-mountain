94fac010cc432b74f15d6837b8f5cc82177e3b4a
Daniel Levy <justsml@users.noreply.github.com>
2017-07-19 15:42:50 -0600

    initial s3 block store example

---
+++ examples/functional-s3-block-store/index.js
@@ -0,0 +1,49 @@
+var minioClient
+const Promise         = require('bluebird')
+const Minio           = require('minio')
+const {blockStorage}  = require('app/config')
+
+module.exports = {set, get, remove, stat,
+  listBuckets, createBucket,
+  signedGetUrl, signedSetUrl}
+
+function set({id, stream, size = 0, contentType = 'application/octet-stream', bucket = 'files'}) {
+  return minioClient.putObjectAsync(bucket, id, stream, size, contentType)
+}
+
+function get({id, bucket = 'files'}) {
+  return minioClient.getObjectAsync(bucket, id)
+}
+
+function remove({id, bucket = 'files'}) {
+  return minioClient.removeObjectAsync(bucket, id)
+}
+
+function stat({id, bucket = 'files'}) {
+  return minioClient.statObjectAsync(bucket, id)
+}
+
+function signedGetUrl({id, expires = 60*60*24, bucket = 'files'}) {
+  return minioClient.presignedGetObjectAsync(bucket, id, expires)
+}
+
+function signedSetUrl({id, expires = 60*60*24, bucket = 'files'}) {
+  return minioClient.presignedPutObjectAsync(bucket, id, expires)
+}
+
+function listBuckets() {
+  return minioClient.listBucketsAsync()
+}
+
+function createBucket(bucket) {
+  return listBuckets()
+    // .tap(console.log.bind(console, 'bucketList'))
+    .then(buckets => buckets.find(({name}) => bucket === name))
+    .then(exists => exists ? true : minioClient.makeBucketAsync(bucket, 'us-east-1'))
+}
+
+if (blockStorage) {
+  blockStorage.port = parseInt(blockStorage.port, null)
+  minioClient = new Minio.Client(blockStorage)
+  Promise.promisifyAll(minioClient)
+}
