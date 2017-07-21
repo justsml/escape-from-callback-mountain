d4a84900afa9d4103c0edf6466b2230a9ef4e6bc
Daniel Levy <justsml@users.noreply.github.com>
2017-07-20 18:15:11 -0600

    Update config.js

--- examples/functional-s3-block-store/config.js
+++ examples/functional-s3-block-store/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  secure:     process.env.S3_SECURE 		!== 'true' ? false : true,
-  port:       process.env.S3_PORT 			|| 9000,
-  endPoint:   process.env.S3_ENDPOINT 	|| 'localhost',
+  secure:     process.env.S3_SECURE     !== 'true' ? false : true,
+  port:       process.env.S3_PORT       || 9000,
+  endPoint:   process.env.S3_ENDPOINT   || 'localhost',
   accessKey:  process.env.S3_ACCESS_KEY || 'admin',
   secretKey:  process.env.S3_SECRET_KEY || 'test',
 }
