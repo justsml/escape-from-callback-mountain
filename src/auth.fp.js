2c443b12ebf66323b0824be8c1839e67c92e098d
Dan Levy <dan@danlevy.net>
2017-08-10 18:37:28 -0600

    fixes to FP example

--- src/auth.fp.js
+++ src/auth.fp.js
@@ -1,30 +1,34 @@
 // EXAMPLE: FUNCTIONAL PROMISES
 const Promise           = require('bluebird')
-const {hashString}      = require('./lib/crypto')
+const {hashStringAsync} = require('./lib/crypto')
 const {logEventAsync}   = require('./lib/log')
-const {getModel}        = require('./lib/db')
+const {getModelAsync}   = require('./lib/db')
 
 module.exports = {auth}
 
 function auth({username, password}) {
   return Promise.resolve({username, password})
-    .then(_isInputValid)
+    .then(_checkInput)
     .tap(logEventAsync({event: 'login', username}))
     .then(_loginUser)
-    .then(_isResultValid)
+    .then(_checkResults)
 }
 
 function _loginUser({username, password}) {
-  return Promise.props({username, password: hashString(password)})
-    .then(params => getModel('users').findOneAsync(params))
+  return Promise.props({
+    Users:      getModelAsync('users'), 
+    hashedPass: hashStringAsync(password)
+  })
+  .then(({Users, hashedPass}) => Users
+    .findOneAsync({username, password: hashedPass})) 
 }
 
-function _isInputValid({username, password}) {
+function _checkInput({username, password}) {
   if (!username || username.length < 1) throw new Error('Invalid username.')
   if (!password || password.length < 6) throw new Error('Invalid password.')
   return {username, password}
 }
 
-function _isResultValid(user) {
+function _checkResults(user) {
   return user && user._id ? user : Promise.reject(new Error('User Not found!'))
 }
