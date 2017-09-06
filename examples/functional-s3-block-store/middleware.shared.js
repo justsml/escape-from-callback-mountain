382db621b37ba2db4d2e0c55f18b8c2b175ef8a5
Dan Levy <dan@danlevy.net>
2017-09-05 22:35:52 -0600

    cleaner middleware bits

--- examples/functional-s3-block-store/middleware.shared.js
+++ examples/functional-s3-block-store/middleware.shared.js
@@ -1,15 +1,11 @@
-const errorNotFound = (req, res) => res.status(404)
-  .send({error: 'Requested file not found. Check your path.'})
-const errorNoArguments = (req, res) => res.status(500)
-  .send({error: 'Please specify a bucket/fileId in URI path.'})
-const errorUnexpected = (req, res) => res.status(500)
-  .send({error: 'Unexpected Error: Check DB connection/configuration.'})
-const isLoggedIn = (req, res, next) => req
-  .user ? next() : res.status(503).send({error: 'Access denied'})
-
 module.exports = {
-  errorNotFound,
-  errorNoArguments,
-  errorUnexpected,
-  isLoggedIn,
+  errorNotFound: (req, res) => res.status(404)
+    .send({error: 'Requested file not found. Check your path.'}),
+  errorNoArguments: (req, res) => res.status(500)
+    .send({error: 'Please specify a bucket/fileId in URI path.'}),
+  errorUnexpected: (req, res) => res.status(500)
+    .send({error: 'Unexpected Error: Check DB connection/configuration.'}),
+
+  isLoggedIn: (req, res, next) => req
+    .user ? next() : res.status(503).send({error: 'Access denied'}),
 }
